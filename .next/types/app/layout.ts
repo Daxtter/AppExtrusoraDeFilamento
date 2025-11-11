dc5fe1cdd288f71e68e47535cf42a79b6acb7ba5
Daxtter <lmnunez2003@gmail.com>
2025-11-10 17:56:00 -0800

    Ahora mas limpio segun

--- .next/types/app/layout.ts
+++ .next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: C:\Users\Simple\Downloads\ProyectoExtractorDeFilamento\extractor-server\src\app\layout.tsx
+// File: /home/manuel/Documentos/9noSemestre/ProyectoDeCarrera/paginawebPC/SuperNeoFilX/src/app/layout.tsx
 import * as entry from '../../../src/app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
